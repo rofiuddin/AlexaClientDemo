7ba8ec4f9feaf81d5b703f324596dfe3d7eb660b
rofiuddin <rofi@FRAMGIAs-Mac-mini.local>
2017-07-11 13:17:38 +0600

    Initial Commit

---
+++ AlexaClientDemo/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  AlexaClientDemo
+//
+//  Created by Rofi Uddin on 7/11/17.
+//  Copyright © 2017 Rofi Uddin. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
