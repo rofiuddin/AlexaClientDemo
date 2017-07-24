49a945b7bf2b342799a3bc434d4a4d0cf9823e97
rofiuddin <md.rofi.uddin@framgia.com>
2017-07-24 09:41:40 +0600

    implement login with amazon feature

---
+++ AlexaClientDemo/LoginWithAmazonProxy.swift
@@ -0,0 +1,27 @@
+//
+//  LoginWithAmazonProxy.swift
+//  AlexaClientDemo
+//
+//  Created by Rofi Uddin on 7/19/17.
+//  Copyright © 2017 Rofi Uddin. All rights reserved.
+//
+
+import Foundation
+import LoginWithAmazon
+
+class LoginWithAmazonProxy {
+    
+    static let sharedInstance = LoginWithAmazonProxy()
+    
+    func login(delegate: AIAuthenticationDelegate) {
+        AIMobileLib.authorizeUser(forScopes: Settings.Credentials.SCOPES, delegate: delegate, options: [kAIOptionScopeData: Settings.Credentials.SCOPE_DATA])
+    }
+    
+    func logout(delegate: AIAuthenticationDelegate) {
+        AIMobileLib.clearAuthorizationState(delegate)
+    }
+    
+    func getAccessToken(delegate: AIAuthenticationDelegate) {
+        AIMobileLib.getAccessToken(forScopes: Settings.Credentials.SCOPES, withOverrideParams: nil, delegate: delegate)
+    }
+}
--- AlexaClientDemo/SecondViewController.swift
+++ AlexaClientDemo/SecondViewController.swift
@@ -7,12 +7,25 @@
 //
 
 import UIKit
+import LoginWithAmazon
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, AIAuthenticationDelegate {
+    @IBOutlet weak var loginWithAmazonBtn: UIButton!
+    @IBOutlet weak var logoutBtn: UIButton!
 
+    let lwa = LoginWithAmazonProxy.sharedInstance
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func onClickLoginWithAmazonBtn(_ sender: Any) {
+        lwa.login(delegate: self)
+    }
+    
+    @IBAction func onClickLogoutBtn(_ sender: Any) {
+        lwa.logout(delegate: self)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +33,26 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func requestDidSucceed(_ apiResult: APIResult) {
+        switch(apiResult.api) {
+        case API.authorizeUser:
+            print("Authorized")
+            lwa.getAccessToken(delegate: self)
+        case API.getAccessToken:
+            print("Login successfully!")
+            LoginWithAmazonToken.sharedInstance.loginWithAmazonToken = apiResult.result as! String?
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let controller = storyboard.instantiateViewController(withIdentifier: "FirstViewController")
+            self.present(controller, animated: true, completion: nil)
+        case API.clearAuthorizationState:
+            print("Logout successfully!")
+        default:
+            return
+        }
+    }
+    
+    func requestDidFail(_ errorResponse: APIError) {
+        print("Error: \(errorResponse.error.message)")
+    }
 }
 
